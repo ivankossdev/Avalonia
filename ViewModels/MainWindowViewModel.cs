0705429846b38cb58ebfbcf1f1fb1ac44f5fde9e
ivankossdev <ivan.koss@yandex.ru>
2026-03-13 08:43:54 +0300

    work window

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace MyFirstAvaloniaApp.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-public partial class MainWindowViewModel : ViewModelBase
+namespace MyFirstAvaloniaApp.ViewModels;
+
+public partial class MainViewModel : ObservableObject
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private string _logText = "История нажатий:\n";
+
+    [RelayCommand]
+    private void Button1()
+    {
+        LogText += "Нажата кнопка 1\n";
+    }
+
+    [RelayCommand]
+    private void Button2()
+    {
+        LogText += "Нажата кнопка 2\n";
+    }
+
+    [RelayCommand]
+    private void Clear()
+    {
+        LogText = "История нажатий:\n";
+    }
 }
